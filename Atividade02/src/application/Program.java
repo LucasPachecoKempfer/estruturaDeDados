3a8ab4ccd275fec2878b246f1b481543e96b72f4
LucasPachecoKempfer <lpachecokempfer@gmail.com>
2026-08-18 19:58:34 -0300

    preencher vetor ordenadamente - incompleto

---
+++ Atividade02/src/application/Program.java
@@ -0,0 +1,20 @@
+package application;
+
+import model.Vetor;
+
+public class Program {
+    static void main() {
+
+        Vetor<Integer> vetor = new Vetor<>(10);
+
+        vetor.preencherOrdenado(10, 50);
+
+
+
+
+
+
+
+    }
+
+}
